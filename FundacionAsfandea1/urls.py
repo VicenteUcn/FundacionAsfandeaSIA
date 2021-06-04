a96ad959d98f4eafbfdc4e88dbc17c4520b66fcd
VicenteUcn <vicente.figueroa@alumnos.ucn.cl>
2021-06-04 08:44:08 -0400

    vistas pt 3
    seccion infantil preliminar

--- FundacionAsfandea1/urls.py
+++ FundacionAsfandea1/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from FundacionAsfandea.views import index
 from django.contrib import admin
 from django.urls import path
+from FundacionAsfandea.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index),
+    path('inicio',index),
+    path('programas',programas),
+    path('infantil',infantil),
+    path('apoderados',apoderados),
+    path('programas',programas),
+     path('calendario',calendario),
 ]
